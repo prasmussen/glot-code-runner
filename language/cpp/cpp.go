b28b57e507809da72eb3b0fc981da503762eaa03
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-04-25 01:38:45 +0200

    Build all source files with clang

--- language/cpp/cpp.go
+++ language/cpp/cpp.go
@@ -9,11 +9,25 @@ func Run(files []string) (string, string, error) {
     workDir := filepath.Dir(files[0])
     binName := "a.out"
 
-    stdout, stderr, err := cmd.Run(workDir, "clang++", files[0], "-o", binName)
+    sourceFiles := filterSourceFiles(files)
+    args := append([]string{"clang++", "-o", binName}, sourceFiles...)
+    stdout, stderr, err := cmd.Run(workDir, args...)
     if err != nil {
         return stdout, stderr, err
     }
 
     binPath := filepath.Join(workDir, binName)
     return cmd.Run(workDir, binPath)
+}
+
+func filterSourceFiles(files []string) []string {
+    var newFiles []string
+
+    for _, file := range files {
+        if filepath.Ext(file) == ".cpp" {
+            newFiles = append(newFiles, file)
+        }
+    }
+
+    return newFiles
 }
