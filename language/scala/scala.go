ffd167294b30dbc2f63fa6c64aef1114c5e6f1e1
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-05-01 17:08:20 +0200

    Compile files first

--- language/scala/scala.go
+++ language/scala/scala.go
@@ -7,5 +7,12 @@ import (
 
 func Run(files []string) (string, string, error) {
     workDir := filepath.Dir(files[0])
-    return cmd.Run(workDir, "scala", "-nocompdaemon", files[0])
+
+    args := append([]string{"scalac"}, files...)
+    stdout, stderr, err := cmd.Run(workDir, args...)
+    if err != nil {
+        return stdout, stderr, err
+    }
+
+    return cmd.Run(workDir, "scala", "Main")
 }
