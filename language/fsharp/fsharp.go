0353649d97285f636c641420c649e721374c871d
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-05-01 16:23:48 +0200

    Compile files in reverse order

--- language/fsharp/fsharp.go
+++ language/fsharp/fsharp.go
@@ -10,7 +10,7 @@ func Run(files []string) (string, string, error) {
     workDir := filepath.Dir(files[0])
     binName := "a.exe"
 
-    sourceFiles := util.FilterByExtension(files, "fs")
+    sourceFiles := reverse(util.FilterByExtension(files, "fs"))
     args := append([]string{"fsharpc", "--out:" + binName}, sourceFiles...)
     stdout, stderr, err := cmd.Run(workDir, args...)
     if err != nil {
@@ -19,4 +19,14 @@ func Run(files []string) (string, string, error) {
 
     binPath := filepath.Join(workDir, binName)
     return cmd.Run(workDir, "mono", binPath)
+}
+
+func reverse(files []string) []string {
+    reversed := make([]string, 0, len(files))
+
+    for i := len(files) - 1; i >= 0; i-- {
+        reversed = append(reversed, files[i])
+    }
+
+    return reversed
 }
