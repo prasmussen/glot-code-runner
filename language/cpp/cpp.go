32755a0e3bcea6b308a8d0c28e50d51f22f6bdd1
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-08-31 14:56:25 +0200

    Use c++11 by default

--- language/cpp/cpp.go
+++ language/cpp/cpp.go
@@ -11,7 +11,7 @@ func Run(files []string) (string, string, error) {
     binName := "a.out"
 
     sourceFiles := util.FilterByExtension(files, "cpp")
-    args := append([]string{"clang++", "-o", binName}, sourceFiles...)
+    args := append([]string{"clang++", "-std=c++11", "-o", binName}, sourceFiles...)
     stdout, stderr, err := cmd.Run(workDir, args...)
     if err != nil {
         return stdout, stderr, err
