d21838c5dd53820b10be07637e509f4aa520592c
Hanwen Wu <steinwaywhw@gmail.com>
2016-05-03 13:40:39 -0400

    add support for running naive ATS code

---
+++ language/ats/ats.go
@@ -0,0 +1,22 @@
+package ats
+
+import (
+    "path/filepath"
+    "../../util"
+    "../../cmd"
+)
+
+func Run(files []string) (string, string, error) {
+    workDir := filepath.Dir(files[0])
+    binName := "a.out"
+
+    sourceFiles := util.FilterByExtension(files, "dats")
+    args := append([]string{"patscc", "-o", binName}, sourceFiles...)
+    stdout, stderr, err := cmd.Run(workDir, args...)
+    if err != nil {
+        return stdout, stderr, err
+    }
+
+    binPath := filepath.Join(workDir, binName)
+    return cmd.Run(workDir, binPath)
+}
