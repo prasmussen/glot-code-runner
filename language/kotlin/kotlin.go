35162ea02b7c9b1670b39e66728c8ac1ad5671e9
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-09-07 13:13:25 +0200

    Kotlin: generate proper class name

--- language/kotlin/kotlin.go
+++ language/kotlin/kotlin.go
@@ -3,6 +3,7 @@ package kotlin
 import (
 	"github.com/prasmussen/glot-code-runner/cmd"
 	"path/filepath"
+	"strings"
 )
 
 func Run(files []string, stdin string) (string, string, error) {
@@ -18,6 +19,11 @@ func Run(files []string, stdin string) (string, string, error) {
 }
 
 func className(fname string) string {
+	if len(fname) < 5 {
+		return fname
+	}
+
 	ext := filepath.Ext(fname)
-	return fname[0 : len(fname)-len(ext)]
+	name := fname[0 : len(fname)-len(ext)]
+	return strings.ToUpper(string(name[0])) + name[1:] + "Kt"
 }
