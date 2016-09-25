5d1b5036f77151427f6104716458293807772439
Petter Rasmussen <petter.rasmussen@gmail.com>
2016-09-25 12:48:04 +0200

    Find and compile all typescript files

--- language/typescript/typescript.go
+++ language/typescript/typescript.go
@@ -2,10 +2,23 @@ package typescript
 
 import (
 	"github.com/prasmussen/glot-code-runner/cmd"
+	"github.com/prasmussen/glot-code-runner/util"
 	"path/filepath"
 )
 
 func Run(files []string, stdin string) (string, string, error) {
 	workDir := filepath.Dir(files[0])
-	return cmd.RunStdin(workDir, stdin, "typescript", "run", files[0])
+	jsName := "a.js"
+
+	// Find all typescript files and build compile command
+	sourceFiles := util.FilterByExtension(files, "ts")
+	args := append([]string{"tsc", "-out", jsName}, sourceFiles...)
+
+	// Compile to javascript
+	stdout, stderr, err := cmd.Run(workDir, args...)
+	if err != nil {
+		return stdout, stderr, err
+	}
+
+	return cmd.RunStdin(workDir, stdin, "node", jsName)
 }
