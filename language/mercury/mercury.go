4c69aafc06c1b7d23b7b9b9466de7b7c1db3f1fd
rightfold <rightfold@gmail.com>
2016-12-30 12:29:07 +0100

    Add Mercury runner

---
+++ language/mercury/mercury.go
@@ -0,0 +1,22 @@
+package mercury
+
+import (
+	"github.com/prasmussen/glot-code-runner/cmd"
+	"github.com/prasmussen/glot-code-runner/util"
+	"path/filepath"
+)
+
+func Run(files []string, stdin string) (string, string, error) {
+	workDir := filepath.Dir(files[0])
+	binName := "a.out"
+
+	sourceFiles := util.FilterByExtension(files, "m")
+	args := append([]string{"mmc", "-o", binName}, sourceFiles...)
+	stdout, stderr, err := cmd.Run(workDir, args...)
+	if err != nil {
+		return stdout, stderr, err
+	}
+
+	binPath := filepath.Join(workDir, binName)
+	return cmd.RunStdin(workDir, stdin, binPath)
+}
